d05cf2f4ec96f88a24e8ba425f52d4df2cce8939
Zenker, Dr. Klaus (FWKE) - 126506 <k.zenker@hzdr.de>
2023-07-19 13:53:24 +0200

    Fix missing include and increase version number.

--- src/EPICS-Backend.cc
+++ src/EPICS-Backend.cc
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <thread>
 
 #include "EPICS-Backend.h"
 #include "EPICS-BackendRegisterAccessor.h"
@@ -19,26 +20,26 @@
 #include "EPICSChannelManager.h"
 typedef boost::tokenizer<boost::char_separator<char>> tokenizer;
 
-extern "C"{
-    boost::shared_ptr<ChimeraTK::DeviceBackend> ChimeraTK_DeviceAccess_createBackend(
-        std::string address, std::map<std::string, std::string> parameters) {
-      return ChimeraTK::EpicsBackend::createInstance(address, parameters);
-    }
-
-    std::vector<std::string> ChimeraTK_DeviceAccess_sdmParameterNames{"map"};
-
-    std::string ChimeraTK_DeviceAccess_version{CHIMERATK_DEVICEACCESS_VERSION};
-
-    std::string backend_name = "epics";
+extern "C" {
+boost::shared_ptr<ChimeraTK::DeviceBackend> ChimeraTK_DeviceAccess_createBackend(
+    std::string address, std::map<std::string, std::string> parameters) {
+  return ChimeraTK::EpicsBackend::createInstance(address, parameters);
 }
 
-namespace ChimeraTK{
+std::vector<std::string> ChimeraTK_DeviceAccess_sdmParameterNames{"map"};
+
+std::string ChimeraTK_DeviceAccess_version{CHIMERATK_DEVICEACCESS_VERSION};
+
+std::string backend_name = "epics";
+}
+
+namespace ChimeraTK {
   EpicsBackend::BackendRegisterer EpicsBackend::backendRegisterer;
 
-  EpicsBackend::EpicsBackend(const std::string &mapfile): _catalogue_filled(false){
+  EpicsBackend::EpicsBackend(const std::string& mapfile) : _catalogue_filled(false) {
     FILL_VIRTUAL_FUNCTION_TEMPLATE_VTABLE(getRegisterAccessor_impl);
     auto result = ca_context_create(ca_enable_preemptive_callback);
-    if (result != ECA_NORMAL) {
+    if(result != ECA_NORMAL) {
       std::stringstream ss;
       ss << "CA error " << ca_message(result) << "occurred while trying to start channel access.";
       throw ChimeraTK::runtime_error(ss.str());
@@ -49,22 +50,20 @@ namespace ChimeraTK{
     _isFunctional = true;
   }
 
-  EpicsBackend::~EpicsBackend(){
+  EpicsBackend::~EpicsBackend() {
     _asyncReadActivated = false;
     ChannelManager::getInstance().cleanup();
-    if(_isFunctional)
-      ca_context_destroy();
+    if(_isFunctional) ca_context_destroy();
   }
 
-  void EpicsBackend::activateAsyncRead()noexcept{
-    if(!_opened || !_isFunctional)
-      return;
+  void EpicsBackend::activateAsyncRead() noexcept {
+    if(!_opened || !_isFunctional) return;
     _asyncReadActivated = true;
   }
 
   template<typename UserType>
-  boost::shared_ptr< NDRegisterAccessor<UserType> > EpicsBackend::getRegisterAccessor_impl(
-      const RegisterPath &registerPathName, size_t numberOfWords, size_t wordOffsetInRegister, AccessModeFlags flags) {
+  boost::shared_ptr<NDRegisterAccessor<UserType>> EpicsBackend::getRegisterAccessor_impl(
+      const RegisterPath& registerPathName, size_t numberOfWords, size_t wordOffsetInRegister, AccessModeFlags flags) {
     RegisterPath path = "EPICS://" + registerPathName;
 
     EpicsBackendRegisterInfo info = _catalogue_mutable.getBackendRegister(registerPathName);
@@ -82,24 +81,29 @@ namespace ChimeraTK{
     if(info._pv->dbfType == DBR_STSACK_STRING || info._pv->dbfType == DBR_CLASS_NAME) base_type = DBR_STRING;
     info._pv->name = (char*)info._caName.c_str();
     //    switch(info._dpfType){
-    switch (base_type){
-//      case DBR_STRING:
-//        return boost::make_shared<EpicsBackendRegisterAccessor<dbr_string_t, UserType>>(path, shared_from_this(), info, flags, numberOfWords, wordOffsetInRegister);
-//        break;
+    switch(base_type) {
+        //      case DBR_STRING:
+        //        return boost::make_shared<EpicsBackendRegisterAccessor<dbr_string_t, UserType>>(path, shared_from_this(), info, flags, numberOfWords, wordOffsetInRegister);
+        //        break;
       case DBR_FLOAT:
-        return boost::make_shared<EpicsBackendRegisterAccessor<dbr_float_t, dbr_time_float, UserType>>(path, shared_from_this(), info, flags, numberOfWords, wordOffsetInRegister);
+        return boost::make_shared<EpicsBackendRegisterAccessor<dbr_float_t, dbr_time_float, UserType>>(
+            path, shared_from_this(), info, flags, numberOfWords, wordOffsetInRegister);
         break;
       case DBR_DOUBLE:
-        return boost::make_shared<EpicsBackendRegisterAccessor<dbr_double_t, dbr_time_double, UserType>>(path, shared_from_this(), info, flags, numberOfWords, wordOffsetInRegister);
+        return boost::make_shared<EpicsBackendRegisterAccessor<dbr_double_t, dbr_time_double, UserType>>(
+            path, shared_from_this(), info, flags, numberOfWords, wordOffsetInRegister);
         break;
       case DBR_CHAR:
-        return boost::make_shared<EpicsBackendRegisterAccessor<dbr_char_t, dbr_time_char, UserType>>(path, shared_from_this(), info, flags, numberOfWords, wordOffsetInRegister);
+        return boost::make_shared<EpicsBackendRegisterAccessor<dbr_char_t, dbr_time_char, UserType>>(
+            path, shared_from_this(), info, flags, numberOfWords, wordOffsetInRegister);
         break;
       case DBR_SHORT:
-        return boost::make_shared<EpicsBackendRegisterAccessor<dbr_short_t, dbr_time_short, UserType>>(path, shared_from_this(), info, flags, numberOfWords, wordOffsetInRegister);
+        return boost::make_shared<EpicsBackendRegisterAccessor<dbr_short_t, dbr_time_short, UserType>>(
+            path, shared_from_this(), info, flags, numberOfWords, wordOffsetInRegister);
         break;
       case DBR_LONG:
-        return boost::make_shared<EpicsBackendRegisterAccessor<dbr_long_t, dbr_time_long, UserType>>(path, shared_from_this(), info, flags, numberOfWords, wordOffsetInRegister);
+        return boost::make_shared<EpicsBackendRegisterAccessor<dbr_long_t, dbr_time_long, UserType>>(
+            path, shared_from_this(), info, flags, numberOfWords, wordOffsetInRegister);
         break;
         //      case DBR_ENUM:
         //        if(dbr_type_is_CTRL(info._dpfType))
@@ -113,7 +117,6 @@ namespace ChimeraTK{
         throw ChimeraTK::runtime_error(std::string("Type ") + std::to_string(info._pv->dbfType) + " not implemented.");
         break;
     }
-
   }
 
   EpicsBackend::BackendRegisterer::BackendRegisterer() {
@@ -121,24 +124,25 @@ namespace ChimeraTK{
     std::cout << "BackendRegisterer: registered backend type epics" << std::endl;
   }
 
-  boost::shared_ptr<DeviceBackend> EpicsBackend::createInstance(std::string /*address*/, std::map<std::string,std::string> parameters) {
+  boost::shared_ptr<DeviceBackend> EpicsBackend::createInstance(
+      std::string /*address*/, std::map<std::string, std::string> parameters) {
     if(parameters["map"].empty()) {
       throw ChimeraTK::logic_error("No map file provided.");
     }
-    return boost::shared_ptr<DeviceBackend> (new EpicsBackend(parameters["map"]));
+    return boost::shared_ptr<DeviceBackend>(new EpicsBackend(parameters["map"]));
   }
 
-  void EpicsBackend::addCatalogueEntry(RegisterPath path, std::shared_ptr<std::string> pvName){
+  void EpicsBackend::addCatalogueEntry(RegisterPath path, std::shared_ptr<std::string> pvName) {
     EpicsBackendRegisterInfo info(path);
     info._pv.reset((pv*)calloc(1, sizeof(pv)));
     info._caName = std::string(*pvName.get());
     info._pv->name = (char*)info._caName.c_str();
     bool channelAlreadyCreated = ChannelManager::getInstance().channelPresent(info._caName);
-    if(!channelAlreadyCreated){
+    if(!channelAlreadyCreated) {
       auto result = ca_create_channel(
           info._pv->name, ChannelManager::channelStateHandler, this, DEFAULT_CA_PRIORITY, &info._pv->chid);
       ChannelManager::getInstance().addChannel(info._pv->chid, info._caName);
-      if(result != ECA_NORMAL){
+      if(result != ECA_NORMAL) {
         std::cerr << "CA error " << ca_message(result) << " occurred while trying to create channel " << info._pv->name
                   << std::endl;
         return;
@@ -168,44 +172,46 @@ namespace ChimeraTK{
     info._accessModes.add(AccessMode::wait_for_new_data);
   }
 
-  void EpicsBackend::fillCatalogueFromMapFile(const std::string &mapfileName){
+  void EpicsBackend::fillCatalogueFromMapFile(const std::string& mapfileName) {
     boost::char_separator<char> sep{"\t ", "", boost::drop_empty_tokens};
     std::string line;
-    std::ifstream mapfile (mapfileName);
-    if (mapfile.is_open()) {
-      while (std::getline(mapfile,line)) {
-        if(line.empty())
-          continue;
+    std::ifstream mapfile(mapfileName);
+    if(mapfile.is_open()) {
+      while(std::getline(mapfile, line)) {
+        if(line.empty()) continue;
         tokenizer tok{line, sep};
         size_t nTokens = std::distance(tok.begin(), tok.end());
-        if (!(nTokens == 2)){
-          std::cerr << "Wrong number of tokens (" << nTokens << ") in mapfile " << mapfileName << " line (-> line is ignored): \n "  << line << std::endl;
+        if(!(nTokens == 2)) {
+          std::cerr << "Wrong number of tokens (" << nTokens << ") in mapfile " << mapfileName
+                    << " line (-> line is ignored): \n " << line << std::endl;
           continue;
         }
         auto it = tok.begin();
 
-        try{
+        try {
           std::shared_ptr<std::string> pathStr = std::make_shared<std::string>(*it);
           RegisterPath path(*(pathStr.get()));
           it++;
           std::shared_ptr<std::string> nodeName = std::make_shared<std::string>(*it);
           addCatalogueEntry(path, nodeName);
-        } catch (std::out_of_range &e){
-          std::cerr << "Failed reading the following line from mapping file " << mapfileName
-              << "\n " << line << std::endl;
+        }
+        catch(std::out_of_range& e) {
+          std::cerr << "Failed reading the following line from mapping file " << mapfileName << "\n " << line
+                    << std::endl;
         }
       }
       mapfile.close();
-    } else {
+    }
+    else {
       ChimeraTK::runtime_error(std::string("Failed reading mapfile: ") + mapfileName);
     }
 
     auto result = ca_flush_io();
-    if(result == ECA_TIMEOUT){
+    if(result == ECA_TIMEOUT) {
       ChimeraTK::runtime_error("Channel setup failed.");
       return;
     }
-    while(!_isFunctional){
+    while(!_isFunctional) {
       std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
     for(auto& reg : _catalogue_mutable) {
@@ -213,12 +219,12 @@ namespace ChimeraTK{
     }
   }
 
-  void EpicsBackend::setException(){
+  void EpicsBackend::setException() {
     //\ToDo: Why I have to check is functional here? If not setException is called constantly and which means _isFunctional will stay false even if reset in the state handler.
-    if(_isFunctional){
-    _isFunctional = false;
-    _asyncReadActivated = false;
-    ChannelManager::getInstance().setException(std::string("Exception reported by another accessor."));
+    if(_isFunctional) {
+      _isFunctional = false;
+      _asyncReadActivated = false;
+      ChannelManager::getInstance().setException(std::string("Exception reported by another accessor."));
     }
   }
-}
+} // namespace ChimeraTK
